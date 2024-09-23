958e425563e229d8ca0b38bd6ffdaaeef2f0d62b
minsung12345 <whss5699@naver.com>
2024-09-23 17:38:17 +0900

    Create M18310.java
    그리디 알고리즘 실버3

---
+++ M18310.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class M18310 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int L[] = new int[N];
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0;i<N;i++) {
+			L[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(L);
+		System.out.println(L[(N-1)/2]);
+		
+		
+		
+		
+	}
+
+
+}
