02b1805427f43a2c539599dca8c0837ac3d8f3c2
minsung12345 <whss5699@naver.com>
2024-09-28 16:31:51 +0900

    Create M1427.java
    내림차순 정렬

---
+++ M1427.java
@@ -0,0 +1,30 @@
+package Test;
+
+import java.util.Scanner;
+
+public class M1427 {
+
+	public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	String str = sc.next();
+	int A[] = new int[str.length()];
+	for(int i =0;i<str.length();i++) {
+		A[i] = Integer.parseInt( str.substring(i,i+1));
+	}
+	for(int i=0;i<str.length();i++) {
+		int max = i;
+		for(int j=i+1;j<str.length();j++) {
+			if(A[j]>A[max]) max =j;
+		}
+		if(A[i]<A[max]) {
+			int tmp = A[i];
+			A[i] = A[max];
+			A[max] = tmp;
+		}
+	}
+	for(int i =0;i<A.length;i++) {
+		System.out.print(A[i]);
+	}
+	}
+
+}
